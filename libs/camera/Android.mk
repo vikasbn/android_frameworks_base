485899c3b2337e0cb21209d06a6f0f25670aa193
nadlabak <pavel@doshaska.net>
2010-10-20 09:11:54 -0700

    revert the generation of both static and shared camera_client and surfaceflinger_client libraries

--- libs/camera/Android.mk
+++ libs/camera/Android.mk
@@ -1,9 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-# although we use BOARD_USES_ECLAIR_LIBCAMERA (so we make it static), we also need
-# the shared libcamera_client and libsurfaceflinger_client for libvendorOMX_ti_omx...
-
 LOCAL_SRC_FILES:= \
 	Camera.cpp \
 	CameraParameters.cpp \
@@ -12,6 +9,8 @@ LOCAL_SRC_FILES:= \
 	ICameraService.cpp
 
 LOCAL_MODULE:= libcamera_client
+
+ifneq ($(BOARD_USES_ECLAIR_LIBCAMERA),true)
 
 LOCAL_SHARED_LIBRARIES := \
 	libcutils \
@@ -27,16 +26,8 @@ endif
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= \
-	Camera.cpp \
-	CameraParameters.cpp \
-	ICamera.cpp \
-	ICameraClient.cpp \
-	ICameraService.cpp
-
-LOCAL_MODULE:= libcamera_client
+else
 
 include $(BUILD_STATIC_LIBRARY)
 
+endif
--- libs/surfaceflinger_client/Android.mk
+++ libs/surfaceflinger_client/Android.mk
@@ -1,9 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-# although we use BOARD_USES_ECLAIR_LIBCAMERA (so we make it static), we also need
-# the shared libcamera_client and libsurfaceflinger_client for libvendorOMX_ti_omx...
-
 LOCAL_SRC_FILES:= \
 	ISurfaceComposer.cpp \
 	ISurface.cpp \
@@ -14,6 +11,8 @@ LOCAL_SRC_FILES:= \
 	SurfaceComposerClient.cpp
 
 LOCAL_MODULE:= libsurfaceflinger_client
+
+ifneq ($(BOARD_USES_ECLAIR_LIBCAMERA),true)
 
 LOCAL_SHARED_LIBRARIES := \
 	libcutils \
@@ -22,24 +21,15 @@ LOCAL_SHARED_LIBRARIES := \
 	libhardware \
 	libui
 
+
 ifeq ($(TARGET_SIMULATOR),true)
     LOCAL_LDLIBS += -lpthread
 endif
 
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES:= \
-	ISurfaceComposer.cpp \
-	ISurface.cpp \
-	ISurfaceFlingerClient.cpp \
-	LayerState.cpp \
-	SharedBufferStack.cpp \
-	Surface.cpp \
-	SurfaceComposerClient.cpp
-
-LOCAL_MODULE:= libsurfaceflinger_client
+else
 
 include $(BUILD_STATIC_LIBRARY)
 
+endif
