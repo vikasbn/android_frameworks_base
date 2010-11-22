22f085d2bd184545f43765901ab82ac9ca26bdf8
nadlabak <pavel@doshaska.net>
2010-11-22 09:34:20 -0800

    add $ character to hw keyboard picker dialog

--- core/java/android/text/method/QwertyKeyListener.java
+++ core/java/android/text/method/QwertyKeyListener.java
@@ -432,13 +432,13 @@ public class QwertyKeyListener extends BaseKeyListener {
         PICKER_SETS.put('n', "\u00F1\u0144\u0148");
         PICKER_SETS.put('o', "\u00F8\u0153\u00F5\u00F2\u00F3\u00F4\u00F6\u014D");
         PICKER_SETS.put('r', "\u0159");
-        PICKER_SETS.put('s', "\u00A7\u00DF\u015B\u0161\u015F");
+        PICKER_SETS.put('s', "\u00A7\u00DF\u015B\u0161\u015F$");
         PICKER_SETS.put('t', "\u0165");
         PICKER_SETS.put('u', "\u00F9\u00FA\u00FB\u00FC\u016F\u016B");
         PICKER_SETS.put('y', "\u00FD\u00FF");
         PICKER_SETS.put('z', "\u017A\u017C\u017E");
         PICKER_SETS.put(KeyCharacterMap.PICKER_DIALOG_INPUT,
-                             "\u2026\u00A5\u2022\u00AE\u00A9\u00B1[]{}\\|");
+                             "\u2026$\u2022\u00AE\u00A9\u00B1[]{}\\|");
         PICKER_SETS.put('/', "\\");
 
         // From packages/inputmethods/LatinIME/res/xml/kbd_symbols.xml
