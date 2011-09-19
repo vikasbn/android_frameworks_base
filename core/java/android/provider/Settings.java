64c9403e114120df521d5a0924c752ab37c69afc
Micha LaQua <micha.laqua@gmail.com>
2011-09-19 21:55:57 +0200

    option to always display battery status on lockscreen (2/3)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2372,6 +2372,12 @@ public final class Settings {
         public static final String QUIET_HOURS_DIM = "quiet_hours_dim";
 
         /**
+         * Whether to always show battery status
+         * @hide
+         */
+        public static final String LOCKSCREEN_ALWAYS_BATTERY = "lockscreen_always_battery";
+
+        /**
          * Whether to allow overscrolling (edge bounce effect on lists)
          * @hide
          */
