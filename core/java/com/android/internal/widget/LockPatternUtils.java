8715eb9e6d0c089cd1b1b701e1be8614ecb0781e
Micha LaQua <micha.laqua@gmail.com>
2011-10-19 18:29:09 +0200

    good morning!

--- core/java/com/android/internal/widget/LockPatternUtils.java
+++ core/java/com/android/internal/widget/LockPatternUtils.java
@@ -43,7 +43,7 @@ import java.io.RandomAccessFile;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.Date;
-import java.util.TimeZone
+import java.util.TimeZone;
 import java.util.Arrays;
 import java.util.List;
 
