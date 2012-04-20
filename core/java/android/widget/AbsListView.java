f7144db999107a6bbb852c9bbe6ccdb52de0a5ed
Vikas B N <vikas@eternaldawn.in>
2012-04-20 18:51:07 +0530

    Backporting the change to make scrolling smoother Commit Id on nadlabak's gingerbread branch : 9828f89ff2e1d557ad36d3691d49be6d85d93ec5
    Change-Id: I1faeb62b203158a7a1d254315702bc18a495cf05

--- core/java/android/widget/AbsListView.java
+++ core/java/android/widget/AbsListView.java
@@ -464,6 +464,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
     private Runnable mClearScrollingCache;
     private int mMinimumVelocity;
     private int mMaximumVelocity;
+    private int mDecacheThreshold;
     
     final boolean[] mIsScrap = new boolean[1];
     
@@ -598,6 +599,7 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         mTouchSlop = configuration.getScaledTouchSlop();
         mMinimumVelocity = configuration.getScaledMinimumFlingVelocity();
         mMaximumVelocity = configuration.getScaledMaximumFlingVelocity();
+        mDecacheThreshold = mMaximumVelocity / 2;
         mDensityScale = getContext().getResources().getDisplayMetrics().density;
     }
 
@@ -2603,6 +2605,11 @@ public abstract class AbsListView extends AdapterView<ListAdapter> implements Te
         }
 
         void start(int initialVelocity) {
+            if (Math.abs(initialVelocity) > mDecacheThreshold) {
+                // For long flings, scrolling cache causes stutter, so don't use it
+                clearScrollingCache();
+            }
+
             int initialY = initialVelocity < 0 ? Integer.MAX_VALUE : 0;
             mLastFlingY = initialY;
             mScroller.fling(0, initialY, 0, initialVelocity,
